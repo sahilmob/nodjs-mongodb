4dbd089255798ddbf238e99e897f72535cf344b3
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-09 17:16:35 +0200

    add logic to get single product

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
@@ -23,6 +24,19 @@ class Product {
 			.find()
 			.toArray()
 			.then(products => products)
+			.catch(err => console.log(err));
+	}
+
+	static findById(id) {
+		const db = getDb();
+		return db
+			.collection("products")
+			.find({ _id: new mongodb.ObjectId(id) })
+			.next()
+			.then(product => {
+				console.log(product);
+				return product;
+			})
 			.catch(err => console.log(err));
 	}
 }
