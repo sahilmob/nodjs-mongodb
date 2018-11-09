999c435832cd1440939dd4deebd27c9ee73b0ab9
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-09 14:55:26 +0200

    Setup basic db connection

--- util/database.js
+++ util/database.js
@@ -1,8 +1,16 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = callback => {
+	MongoClient.connect(
+		"mongodb+srv://sahil:EgmguZoNUpemFdsF@cluster0-ax6dp.mongodb.net/test?retryWrites=true",
+		{ useNewUrlParser: true }
+	)
+		.then(result => {
+			console.log("DB Connected");
+			callback(result);
+		})
+		.catch(err => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
