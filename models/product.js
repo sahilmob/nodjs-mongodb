ed9db303276356b740572c08665c4606a18ceed1
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-09 19:37:02 +0200

    add logic to edit product

--- models/product.js
+++ models/product.js
@@ -2,17 +2,24 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
-	constructor(title, price, imageUrl, description) {
+	constructor(title, price, imageUrl, description, id) {
 		this.title = title;
 		this.price = price;
 		this.imageUrl = imageUrl;
 		this.description = description;
+		this._id = id;
 	}
 	save() {
 		const db = getDb();
-		return db
-			.collection("products")
-			.insertOne(this)
+		let dbOp; //db operation
+		if (this._id) {
+			dbOp = db
+				.collection("products")
+				.updateOne({ _id: new mongodb.ObjectID(this._id) }, { $set: this });
+		} else {
+			dbOp = db.collection("products").insertOne(this);
+		}
+		return dbOp
 			.then(result => console.log(result))
 			.catch(err => console.log(err));
 	}
